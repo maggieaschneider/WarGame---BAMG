a9a1eeb14e80f24f9f0709c9c453dfba56fae071
Grant Lewison <60186503+glew2@users.noreply.github.com>
2020-01-27 11:26:18 -0500

    Add files via upload
    Manager program. Will contain a chronological guideline to the flow of the program.

---
+++ WarGame_Manager.py
@@ -0,0 +1,19 @@
+from tkinter import *
+# any other
+class WarGame_Manager(object):
+    def __init__(self):
+        self.root = Tk()
+        self.screen = None
+        self.player = None
+        self.computer = None
+    '''
+    Screen 1: Start Screen
+        - 
+    '''
+
+def main():
+    war = WarGame_Manager()
+    war.(# name of first screen, calls other screen in a function in class)
+    war.root.mainloop()
+
+main()
