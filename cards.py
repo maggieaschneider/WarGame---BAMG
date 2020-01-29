52959332e6358bd3043bbffbd3083b783a4a7792
Grant Lewison <glewisonrv@gmail.com>
2020-01-29 11:31:12 -0500

    Images should load in, worked on the card object

--- cards.py
+++ cards.py
@@ -4,8 +4,19 @@
 class card (object):
 
     def __init__(self, image):
-
         self.image = image
+        self.value = self.findValue()
+        self.suit = self.findSuit()
+
+    def findValue(self):
+        # must include if statements for each royal card, J=11, etc.
+        # rest of cards, find first
+        return 0 # change this to become the value (int)
+
+    def findSuit(self):
+        # find the second letter of the name of the image and it will choose a class based on that
+        # probably best to do if statemtents
+        return 0 # change this to become the suit (str (letter))
 
     def war(self, enemy):
         pass
