d26ddde707e62284d5856292011573f6ab4c27b3
Grant Lewison <glewisonrv@gmail.com>
2020-01-31 11:17:19 -0500

    resized back card

--- cards.py
+++ cards.py
@@ -6,11 +6,19 @@ class card (object):
         self.suit = self.findSuit()
 
     def findValue(self):
-        # must include if statements for each royal card, J=11, etc.
-        # rest of cards, find first
-        return 0 # change this to become the value (int)
+        val = str(card.image)[0]
+        if val == "J":
+            val = 11
+        elif val == "Q":
+            val = 12
+        elif val == "K":
+            val = 12
+        elif val == "A":
+            val = 13
+        return int(val) # STILL NEEDS TO BE PUSHED
 
     def findSuit(self):
+        suit = str(card.image)[1]
         # find the second letter of the name of the image and it will choose a class based on that
         # probably best to do if statements
         return 0 # change this to become the suit (str (letter))
@@ -29,9 +37,5 @@ class card (object):
 
 class CardList(object):
     def __init__(self, file_name):
-
         self.card_list = []
-
         text_file = open(file_name, "r")
-
-
