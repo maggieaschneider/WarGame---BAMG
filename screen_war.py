5374b11c37765b8a17e15799f06f779b4dba4425
Grant Lewison <glewisonrv@gmail.com>
2020-02-11 20:59:29 -0500

    1: 2 now beats Ace. 2: Guidelines for how we will work on tie screen was made, should be simple from here.

--- screen_war.py
+++ screen_war.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from cards import CardList
+import time
 class Screen_war(Frame):
     def __init__(self, master, call_on_next, player1, player2):
         super(Screen_war, self).__init__(master)
@@ -28,8 +29,8 @@ class Screen_war(Frame):
 
     def create_widgets(self):
         ''' This method creates all of the widgets for the battle page. '''
-        Label(self, text="You", font = "COMIC 10"
-              ).grid(row=1, column=0)
+        Label(self, text="You", font = "COMIC 10").grid(row=1, column=0)
+        Label(self, text="Your Card", font="COMIC 10").grid(row=1, column=1)
 
         image = PhotoImage(file='cardBack.png')
         p = Label(self, image=image)
@@ -38,6 +39,7 @@ class Screen_war(Frame):
         p.grid(row=2, column=0, sticky=W)
 
         Label(self, text="Computer", font = "COMIC 10").grid(row=1, column=3)
+        Label(self, text="Computer's Card", font="COMIC 10").grid(row=1, column=2)
 
         image = PhotoImage(file="cardBack.png")
         p = Label(self, image=image)
@@ -72,28 +74,46 @@ class Screen_war(Frame):
         p2 = Label(self, image=image)
         p2.photo = image
         p2.grid(row=2, column=2)
-
-        if p1card.value > p2card.value:
-            self.ppoints += 1
-            self.ppoint_label['text'] = "You:", self.ppoints
-            self.p1list.remove(p1card)
-            self.p1list.append(p1card)
-            self.p2list.remove(p2card)
-            self.p1list.append(p2card)
-        elif p1card.value < p2card.value:
+        if p1card.value==14 and p2card.value==2:
             self.cpoints += 1
             self.cpoint_label['text'] = "Computer:", self.cpoints
             self.p2list.remove(p2card)
             self.p2list.append(p2card)
             self.p1list.remove(p1card)
             self.p2list.append(p1card)
-        else:
+        elif p2card.value==14 and p1card==2:
+            self.ppoints += 1
+            self.ppoint_label['text'] = "You:", self.ppoints
             self.p1list.remove(p1card)
             self.p1list.append(p1card)
             self.p2list.remove(p2card)
-            self.p2list.append(p2card)
-
+            self.p1list.append(p2card)
+        else:
+            if p1card.value > p2card.value:
+                self.ppoints += 1
+                self.ppoint_label['text'] = "You:", self.ppoints
+                self.p1list.remove(p1card)
+                self.p1list.append(p1card)
+                self.p2list.remove(p2card)
+                self.p1list.append(p2card)
+            elif p1card.value < p2card.value:
+                self.cpoints += 1
+                self.cpoint_label['text'] = "Computer:", self.cpoints
+                self.p2list.remove(p2card)
+                self.p2list.append(p2card)
+                self.p1list.remove(p1card)
+                self.p2list.append(p1card)
+            else: # this is where we will do work on the tie screen
+                # will require re-griding of p1 and p2 to fit new cards
+                # have to figure out a way to show back card but make it have the value
+                self.p1list.remove(p1card) # change all of these
+                self.p1list.append(p1card) # ^
+                self.p2list.remove(p2card) # ^
+                self.p2list.append(p2card) # ^
+                time.sleep(1)
         if self.ppoints >= 15:
+            time.sleep(0.5) # ASK WANG HOW TO GET IT TO ONLY SLEEP ONE PART OF THE PROGRAM
             self.call_on_selected("p")
         if self.cpoints >= 15:
+            time.sleep(0.5)
             self.call_on_selected("c")
